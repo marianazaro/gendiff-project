331df12678601f5ccff530d9897afaacedc14683
maria <marianazarova892@gmail.com>
2023-03-31 16:12:16 +0200

    linter error correction

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,38 +20,54 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+// eslint-disable-next-line no-undef
 test('testing result for tree files stylish/json', () => {
   const expected = readFile('testFile.txt');
   const actual = genDiff(getFixturePath('treefile1.json'), getFixturePath('treefile2.json'), 'stylish');
+  // eslint-disable-next-line no-undef
   expect(actual).toBe(expected);
 });
+// eslint-disable-next-line no-undef
 test('testing result for tree files stylish/yml-yaml', () => {
   const expected = readFile('testFile.txt');
   const actual = genDiff(getFixturePath('treefile1.yml'), getFixturePath('treefile2.yaml'));
+  // eslint-disable-next-line no-undef
   expect(actual).toBe(expected);
 });
+// eslint-disable-next-line no-undef
 test('testing result for tree files plain/json', () => {
   const expected = readFile('plainFile.txt');
   const actual = genDiff(getFixturePath('treefile1.json'), getFixturePath('treefile2.json'), 'plain');
+  // eslint-disable-next-line no-undef
   expect(actual).toBe(expected);
 });
+// eslint-disable-next-line no-undef
 test('testing result for tree files json/json', () => {
   const expected = readFile('jsonFile.txt');
   const actual = genDiff(getFixturePath('treefile1.json'), getFixturePath('treefile2.json'), 'json');
+  // eslint-disable-next-line no-undef
   expect(actual).toBe(expected);
 });
+// eslint-disable-next-line no-undef
 test('testing throw parsers', () => {
+  // eslint-disable-next-line no-undef
   expect(() => parsers('mjs')).toThrow('Unknown format: mjs');
 });
+// eslint-disable-next-line no-undef
 test('testing throw formatters', () => {
   const a = [{ type: '1' }];
+  // eslint-disable-next-line no-undef
   expect(() => format(a, 2)).toThrow('Unknown format: 2');
 });
+// eslint-disable-next-line no-undef
 test('testing stylish for throw wrong type', () => {
   const a = [{ type: 'sam' }];
+  // eslint-disable-next-line no-undef
   expect(() => stylish(a)).toThrow(new Error('This type does not exist: sam'));
 });
+// eslint-disable-next-line no-undef
 test('testing plain for throw wrong type', () => {
   const a = [{ type: 'samo' }];
+  // eslint-disable-next-line no-undef
   expect(() => plain(a)).toThrow(new Error('This type does not exist: samo'));
 });
